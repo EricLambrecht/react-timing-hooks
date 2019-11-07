c2b766ed28aa7ac4056d6e761c76c616e114d083
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2019-11-07 23:52:52 +0100

    refactor: ♻️ Moved hooks into feature-based subfolders

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import useTimeoutEffect from './useTimeoutEffect'
-import useInterval from './useInterval'
+import useTimeoutEffect from './timeout/useTimeoutEffect'
+import useInterval from './interval/useInterval'
 import useIdleCallbackEffect from './idle-callback/useIdleCallbackEffect'
 
 export { useTimeoutEffect, useInterval, useIdleCallbackEffect }
--- src/timeout/types.ts
+++ src/timeout/types.ts
@@ -3,5 +3,3 @@ export type TimeoutCreator = (handler: () => unknown, timeout: number) => any
 export type TimeoutEffectCallback = (
   timeoutFunc: TimeoutCreator
 ) => void | (() => void | undefined)
-
-export type IntervalCallback = () => unknown
