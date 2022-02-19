8ee31e0ac40d4222a25d0844a3ba11f14da027f0
Eric Lambrecht <ericlambrecht.hh@gmail.com>
2022-02-19 15:44:37 +0100

    test: 🚨 Add a test that uncovers the bug described in issue #25
    references issue #25

--- integration-tests/useAnimationFrameLoop.test.tsx
+++ integration-tests/useAnimationFrameLoop.test.tsx
@@ -1,8 +1,11 @@
-import { render, act, fireEvent } from '@testing-library/react'
+import { render, act, fireEvent, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import React, { useState } from 'react'
 import { useAnimationFrameLoop } from '../.tmp/index'
 // @ts-ignore
 import { animationFrame } from '@shopify/jest-dom-mocks'
+
+jest.useRealTimers()
 
 const TestComponent: React.FC = () => {
   const [count, setCount] = useState(0)
@@ -23,51 +26,103 @@ const TestComponent: React.FC = () => {
 }
 
 describe('useAnimationFrameLoop() Integration Test', () => {
-  beforeAll(() => {
-    animationFrame.mock()
+  describe('with mocked animation frame', () => {
+    beforeAll(() => {
+      animationFrame.mock()
+    })
+
+    afterAll(() => {
+      animationFrame.restore()
+    })
+
+    it('increases the counter on every animation frame', () => {
+      const { getByTestId } = render(<TestComponent />)
+
+      for (let i = 1; i < 10; i++) {
+        act(() => {
+          animationFrame.runFrame()
+        })
+        expect(getByTestId('output').textContent).toBe(i.toString())
+      }
+    })
+
+    it('stops increasing counter if stop is pressed, resumes after second click', () => {
+      const { getByTestId } = render(<TestComponent />)
+
+      for (let i = 1; i <= 10; i++) {
+        act(() => {
+          animationFrame.runFrame()
+        })
+        expect(getByTestId('output').textContent).toBe(i.toString())
+      }
+
+      fireEvent.click(getByTestId('button')) // stop
+
+      for (let i = 11; i <= 20; i++) {
+        act(() => {
+          animationFrame.runFrame()
+        })
+        expect(getByTestId('output').textContent).toBe('10')
+      }
+
+      fireEvent.click(getByTestId('button')) // resume
+
+      for (let i = 11; i <= 20; i++) {
+        act(() => {
+          animationFrame.runFrame()
+        })
+        expect(getByTestId('output').textContent).toBe(i.toString())
+      }
+    })
   })
 
-  afterAll(() => {
-    animationFrame.restore()
-  })
+  describe('Github issue #25: Infinite rerender bug', () => {
+    const UnnecessaryRerenderTest: React.FC<{
+      onRender: jest.Mock
+      pausedOnStartup: boolean
+    }> = ({ onRender, pausedOnStartup }) => {
+      const [count, setCount] = useState(0)
+      const [paused, setPaused] = useState(pausedOnStartup)
 
-  it('increases the counter on every animation frame', () => {
-    const { getByTestId } = render(<TestComponent />)
+      useAnimationFrameLoop(() => {
+        setCount(count + 1)
+      }, paused)
 
-    for (let i = 1; i < 10; i++) {
-      act(() => {
-        animationFrame.runFrame()
-      })
-      expect(getByTestId('output').textContent).toBe(i.toString())
-    }
-  })
+      onRender(paused)
 
-  it('stops increasing counter if stop is pressed, resumes after second click', () => {
-    const { getByTestId } = render(<TestComponent />)
-
-    for (let i = 1; i <= 10; i++) {
-      act(() => {
-        animationFrame.runFrame()
-      })
-      expect(getByTestId('output').textContent).toBe(i.toString())
-    }
-
-    fireEvent.click(getByTestId('button')) // stop
-
-    for (let i = 11; i <= 20; i++) {
-      act(() => {
-        animationFrame.runFrame()
-      })
-      expect(getByTestId('output').textContent).toBe('10')
+      return (
+        <>
+          <p data-testid="output">{count}</p>
+          <button data-testid="button" onClick={() => setPaused(!paused)}>
+            Toggle Pause
+          </button>
+        </>
+      )
     }
 
-    fireEvent.click(getByTestId('button')) // resume
+    it('does not cause an infinite render loop when paused on startup', async () => {
+      const renderMock = jest.fn()
+      render(<UnnecessaryRerenderTest onRender={renderMock} pausedOnStartup />)
+      const promise = new Promise<void>((r) => setTimeout(r, 250))
+      await act(() => promise)
+      expect(renderMock).toBeCalledTimes(1)
+    })
 
-    for (let i = 11; i <= 20; i++) {
-      act(() => {
-        animationFrame.runFrame()
-      })
-      expect(getByTestId('output').textContent).toBe(i.toString())
-    }
+    it('does not cause an infinite render loop when paused is activated later on', async () => {
+      const renderMock = jest.fn()
+      render(
+        <UnnecessaryRerenderTest
+          onRender={renderMock}
+          pausedOnStartup={false}
+        />
+      )
+      const pauseButton = screen.getByRole('button', { name: 'Toggle Pause' })
+      await userEvent.click(pauseButton)
+      const promise = new Promise<void>((r) => setTimeout(r, 250))
+      await act(() => promise)
+      expect(
+        renderMock.mock.calls.filter((args) => args[0] === true) // rendere call where paused was true
+      ).toHaveLength(1)
+    })
   })
 })
