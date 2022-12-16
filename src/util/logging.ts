fc9d8dcc087b8bfe41413226b873243bc26edda5
Eric Lambrecht <mail@ericlambrecht.de>
2022-12-16 22:19:21 +0100

    refactor(logging): Add logging utility functions

---
+++ src/util/logging.ts
@@ -0,0 +1,13 @@
+const LIB_NAME = 'React Timing Hooks'
+
+const buildMessage = (message: string) => {
+  return `${LIB_NAME}: ${message}`
+}
+
+export const log = (logFunction: 'log' | 'warn' | 'error', message: string) => {
+  console[logFunction](buildMessage(message))
+}
+
+export const logInfo = (message: string) => log('log', message)
+export const logError = (message: string) => log('error', message)
+export const logWarning = (message: string) => log('warn', message)
